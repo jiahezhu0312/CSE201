f2e29d038ddf7cdf9cb69539246e3178d83bdaa5
jiahezhu0312 <jiahe.zhu@polytechnique.edu>
2019-10-10 11:06:19 +0200

    More advanced Complex class
    A Classs of complex number with more operations

---
+++ Complex_Ex2.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cmath>
+class Complex {
+private:
+  double real;        // the real part
+  double imaginary;   // the imaginary part
+
+  public:
+    // construct the complex number zero
+    Complex(){
+        real=0;
+        imaginary=0;
+    };
+
+    // construct a complex number with given real and imaginary parts
+    Complex(double real, double imaginary){
+        this->real=real;
+        this->imaginary=imaginary;
+    };
+
+    ~Complex(){
+        std::cout <<"Destroying number "<<real<<" + "<<imaginary <<" * i"<< std::endl;
+        };
+
+    double getReal(){return real;};             // get the real part of the number
+    double getImaginary(){return imaginary;};        // get the imaginary part of the number
+    void setReal(double re){real=re;};      // set the value of the real part
+    void setImaginary(double im){imaginary=im;}; // set the value of the imaginary part
+
+    double magnitude(){
+        double re2;
+        double im2;
+
+        re2=  pow (real,2);
+        im2=pow (imaginary,2);
+        return sqrt(re2+im2);
+
+    };           // computes the distance from the complex origin
+
+/*
+   modifies the number so its magnitude becomes 1
+   but it stays in the same direction from the complex origin, if possible. In other
+   words, project the original number onto the unit circle. If that operation is not possible, the complex number must be left unchanged.
+*/
+    void normalize(){
+        double mag=magnitude();
+        real = real/mag;
+        imaginary=imaginary/mag;
+    };
+
+};
+
+int main() {
+  Complex c1;
+  Complex c2 = Complex(3,4);
+
+  c1.normalize();
+
+  c1.setReal(c2.getReal());
+  c1.setImaginary(c2.getImaginary());
+
+  std::cout << "c1 magnitude: " << c1.magnitude() << std::endl;
+  c1.normalize();
+
+  std::cout << "c1 is: " << c1.getReal() <<
+    " + " << c1.getImaginary() << " * i" << std::endl;
+  std::cout << "c1 magnitude:" << c1.magnitude() << std::endl;
+}
