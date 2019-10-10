28131fd84493e28c88f40c4d01cf0698cb6dcf6a
jiahezhu0312 <jiahe.zhu@polytechnique.edu>
2019-10-10 10:46:21 +0200

    My first classe
    A class of complex numbers

---
+++ Complex_Ex1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+class Complex {
+private:
+  double real;        // the real part
+  double imaginary;   // the imaginary part
+
+  public:
+    // construct the complex number zero
+    Complex(){
+        real=0;
+        imaginary=0;
+    };
+
+    // construct a complex number with given real and imaginary parts
+    Complex(double real, double imaginary){
+        this->real=real;
+        this->imaginary=imaginary;
+    };
+
+    ~Complex(){
+        std::cout <<"Destroying number "<<real<<" + "<<imaginary <<" * i"<< std::endl;
+        };
+
+};
+
+int main(){
+    Complex a;
+    Complex b(3,0);
+    Complex c(1,2);
+
+    return 0;
+}
